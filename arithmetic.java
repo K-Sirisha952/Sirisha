7e0c7119cd8dcfcb4fc626e7e892aeda22b20f28
K-Sirisha952 <53805053+K-Sirisha952@users.noreply.github.com>
2019-08-06 08:41:52 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,19 @@
+class one
+{
+public static void main(String ar[])
+{
+int add,sub,mul,div;
+float a=10.5f;
+int b=5;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+System.out.println(add);
+System.out.println(sub);
+System.out.println(mul);
+System.out.println(div);
+System.out.println(mod);
+}
+}
